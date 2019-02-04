0cf06f352c1ee285452ae881b699016e38247633
Maria Dubyaga <kraftaa@gmail.com>
2019-02-04 11:47:04 -0500

    event mouse and double click

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ new Vue({
     // root instance to control
     el:'#vue-app',
     data: {
-        age: 18
+        age: 18,
+        x: 0,
+        y: 0
     },
     methods: {
-
+        add: function(inc){
+           this.age += inc;
+        },
+        subtract: function(dec){
+           this.age -= dec;
+        },
+        updateXY: function(event){
+            // console.log(event);
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
     }
 
 });
