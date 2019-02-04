8a93a0810a0f0c8a9b6af5d535bbe4484241a0ef
Maria Dubyaga <kraftaa@gmail.com>
2019-02-04 11:28:14 -0500

    events1

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ new Vue({
     // root instance to control
     el:'#vue-app',
     data: {
-        name: 'Maria',
-        job: 'Developer',
-        site: 'https://github.com/kraftaa',
-        siteTag:'<a href="https://github.com/kraftaa">Site Tag</a>'
+        age: 18
     },
     methods: {
-        greet: function(time){
 
-            return 'Good '+ time + ' ' + this.name;
-        }
     }
+
 });
