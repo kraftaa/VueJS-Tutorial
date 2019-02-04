2f1b00874a6211a6ee1879fdcd8e2e56417c64ca
Maria Dubyaga <kraftaa@gmail.com>
2019-02-04 10:44:12 -0500

    data binding

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ new Vue({
     // root instance to control
     el:'#vue-app',
     data: {
-        name: 'Maria'
+        name: 'Maria',
+        job: 'Developer',
+        site: 'https://github.com/kraftaa'
+    },
+    methods: {
+        greet: function(time){
+
+            return 'Good '+ time + ' ' + this.name;
+        }
     }
 });
