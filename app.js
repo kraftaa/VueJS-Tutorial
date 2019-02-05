ec85f640a37347f73ed2d0bdc202336351936ea3
Maria Dubyaga <kraftaa@gmail.com>
2019-02-05 09:12:48 -0500

    computed dynamic CSS classes

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 new Vue({
-    // properties
-    // root instance to control
-    el:'#vue-app',
-    data: {
-        age: 20,
-        a:0,
-        b:0
-    },
-    methods: {
-        // addToA: function(){
-        //     console.log('addtoA');
-        //     return this.a + this.age;
-        // },
-        // addToB: function(){
-        //      console.log('addtoB');
-        //     return this.b + this.age;
-        // }
-    },
-    computed: {
-        addToA: function(){
-            console.log('addtoA');
-            return this.a + this.age;
-        },
-        addToB: function(){
-             console.log('addtoB');
-            return this.b + this.age;
-        }
+  // properties
+  // root instance to control
+  el:'#vue-app',
+  data: {
+    available:false,
+    nearby:false
+
+  },
+  methods: {
+
+  },
+  computed: {
+    compClasses: function() {
+      return{
+        available: this.available,
+        nearby: this.nearby
+      }
     }
+}
 
 });
