f91c501bdf1823121d3b5fc818d4e1821ed422e0
Maria Dubyaga <kraftaa@gmail.com>
2019-02-05 23:33:29 -0500

    Intro to components

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
-var one = new Vue({
-  // properties
-  // root instance to control
-  el:'#vue-app-one',
-  data: {
-    title:'Vue App One'
+Vue.component('greeting', {
+  template:'<p>Hey there, I am {{ name }}. <button v-on:click="changeName">Change name</button></p>',
+  data: function(){
+    return{
+      name:"Yoshi"
+    }
   },
   methods: {
-
-  },
-  computed: {
-    greet: function(){
-      return "Hello from app one";
+    changeName:function(){
+      this.name='Mario';
     }
   }
 
 });
 
-var two = new Vue({
+
+new Vue({
+  // properties
+  // root instance to control
+  el:'#vue-app-one',
+  data: {
+
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+
+});
+
+new Vue({
   // properties
   // root instance to control
   el:'#vue-app-two',
   data: {
-    title:'Vue App Two'
+
   },
   methods: {
-    changeTitle: function(){
-      one.title = "title changed";
-    }
+
   },
   computed: {
-    greet: function(){
-      return "Yo this is app two";
     }
-  }
+
 
 });
 
