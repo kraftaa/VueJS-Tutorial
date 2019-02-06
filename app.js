1611494200adf2e14dc2551ad8d8263a1a9833a4
Maria Dubyaga <kraftaa@gmail.com>
2019-02-05 22:42:24 -0500

    multiple vue instances

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-new Vue({
+var one = new Vue({
   // properties
   // root instance to control
-  el:'#vue-app',
+  el:'#vue-app-one',
   data: {
-    characters:['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-    ninjas: [
-      {name: 'Ryu', age: 25},
-      {name: 'Yoshi', age: 35},
-      {name: 'Ken', age: 55}
-    ]
+    title:'Vue App One'
   },
   methods: {
 
   },
   computed: {
-
+    greet: function(){
+      return "Hello from app one";
+    }
   }
 
 });
+
+var two = new Vue({
+  // properties
+  // root instance to control
+  el:'#vue-app-two',
+  data: {
+    title:'Vue App Two'
+  },
+  methods: {
+    changeTitle: function(){
+      one.title = "title changed";
+    }
+  },
+  computed: {
+    greet: function(){
+      return "Yo this is app two";
+    }
+  }
+
+});
+
+
+two.title = "Changed from outside";
